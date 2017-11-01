f0e2be50bde9d3adbb64fe1c37ad26e9b69459a7
masonp <masonp@seattleu.edu>
2017-11-01 17:24:08 -0400

    encapsulated

--- js/main.js
+++ js/main.js
@@ -15,78 +15,78 @@ class Student extends Person {
     super(name, email);
     this.attendance = [];
   }
-//Attendance percentage output
-calculateAttendance(){
-  if (this.attendance.length > 0) {
-  let counter = 0;
-  for (let mark of this.attendance) {
-    counter = counter + mark;
+  //Attendance percentage output
+  calculateAttendance() {
+    if (this.attendance.length > 0) {
+      let counter = 0;
+      for (let mark of this.attendance) {
+        counter = counter + mark;
+      }
+      let attendancePercentage = counter / this.attendance.length * 100;
+      return `${attendancePercentage}%`;
+    } else {
+      return "0%";
+    }
   }
-  let attendancePercentage = counter / this.attendance.length * 100;
-  return `${attendancePercentage}%`;
-} else {
-  return "0%";
-}
-}
 }
 //Adds a teacher email and honorific
 class Teacher extends Person {
   constructor(name, email, honorific) {
-  super(name, email, honorific);
-  this.honorific = honorific;
-}
+    super(name, email, honorific);
+    this.honorific = honorific;
+  }
 }
 //Runs the roster
 class Course {
-    constructor(courseCode, courseTitle, courseDescription){
-        this.code = courseCode;
-        this.title = courseTitle;
-        this.description = courseDescription;
-        this.teacher = null;
-        this.students = [];
-    }
-//Prompts for student name and email and updates roster
-addStudent(){
-  let name = prompt('Student Full Name:');
-  let email = prompt('Student Email:');
-  let newStudent = new Student(name, email);
-  this.students.push(newStudent);
-  updateRoster(this);
-}
-//Prompts for teacher name and email
-setTeacher(){
-  let name = prompt('Teacher Full Name:');
-  let email = prompt('Teacher Email:');
-  let honorific = prompt('Honorific (e.g. Dr., Prof., Mr., Mrs.,):');
-  this.teacher = new Teacher(name, email, honorific);
-  updateRoster(this);
-}
-//Record attendance in roster
-markAttendance(username, status='present'){
-  let student = this.findStudent(username);
-  if (status == 'present'){
-    student.attendance.push(1);
-  } else {
-    student.attendance.push(0);
+  constructor(courseCode, courseTitle, courseDescription) {
+    this.code = courseCode;
+    this.title = courseTitle;
+    this.description = courseDescription;
+    this.teacher = null;
+    this.students = [];
   }
-  updateRoster(this);
-}
-}
-
-
-    //////////////////////////////////////////////
-    // Methods provided for you -- DO NOT EDIT /////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////
-
-    findStudent(username){
-        // This method provided for convenience. It takes in a username and looks
-        // for that username on student objects contained in the `this.students`
-        // Array.
-        let foundStudent = this.students.find(function(student, index){
-            return student.username == username;
-        });
-        return foundStudent;
+  //Prompts for student name and email and updates roster
+  addStudent() {
+    let name = prompt('Student Full Name:');
+    let email = prompt('Student Email:');
+    let newStudent = new Student(name, email);
+    this.students.push(newStudent);
+    updateRoster(this);
+  }
+  //Prompts for teacher name and email
+  setTeacher() {
+    let name = prompt('Teacher Full Name:');
+    let email = prompt('Teacher Email:');
+    let honorific = prompt('Honorific (e.g. Dr., Prof., Mr., Mrs.,):');
+    this.teacher = new Teacher(name, email, honorific);
+    updateRoster(this);
+  }
+  //Record attendance in roster
+  markAttendance(username, status = 'present') {
+    let student = this.findStudent(username);
+    if (status == 'present') {
+      student.attendance.push(1);
+    } else {
+      student.attendance.push(0);
     }
+    updateRoster(this);
+  }
+
+
+
+  //////////////////////////////////////////////
+  // Methods provided for you -- DO NOT EDIT /////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////
+
+  findStudent(username) {
+    // This method provided for convenience. It takes in a username and looks
+    // for that username on student objects contained in the `this.students`
+    // Array.
+    let foundStudent = this.students.find(function(student, index) {
+      return student.username == username;
+    });
+    return foundStudent;
+  }
 }
 
 //Prompt for course code
@@ -112,12 +112,12 @@ rosterTitle.innerHTML = `${myCourse.code}: ${myCourse.title}`;
 let rosterDescription = document.querySelector('#course-description');
 rosterDescription.innerHTML = myCourse.description;
 
-if (myCourse.teacher){
-    let rosterTeacher = document.querySelector('#course-teacher');
-    rosterTeacher.innerHTML = `${myCourse.teacher.honorific} ${myCourse.teacher.name}`;
+if (myCourse.teacher) {
+  let rosterTeacher = document.querySelector('#course-teacher');
+  rosterTeacher.innerHTML = `${myCourse.teacher.honorific} ${myCourse.teacher.name}`;
 } else {
-    let rosterTeacher = document.querySelector('#course-teacher');
-    rosterTeacher.innerHTML = "Not Set";
+  let rosterTeacher = document.querySelector('#course-teacher');
+  rosterTeacher.innerHTML = "Not Set";
 }
 
 let rosterTbody = document.querySelector('#roster tbody');
@@ -126,89 +126,88 @@ rosterTbody.innerHTML = '';
 
 // Create event listener for adding a student.
 let addStudentButton = document.querySelector('#add-student');
-addStudentButton.addEventListener('click', function(e){
-    console.log('Calling addStudent() method.');
-    myCourse.addStudent();
+addStudentButton.addEventListener('click', function(e) {
+  console.log('Calling addStudent() method.');
+  myCourse.addStudent();
 })
 
 // Create event listener for adding a teacher.
 let addTeacherButton = document.querySelector('#add-teacher');
-addTeacherButton.addEventListener('click', function(e){
-    console.log('Calling setTeacher() method.');
-    myCourse.setTeacher();
+addTeacherButton.addEventListener('click', function(e) {
+  console.log('Calling setTeacher() method.');
+  myCourse.setTeacher();
 })
 
 // Call Update Roster to initialize the content of the page.
 updateRoster(myCourse);
 
-function updateRoster(course){
-    let rosterTbody = document.querySelector('#roster tbody');
-    // Clear Roster Content
-    rosterTbody.innerHTML = '';
-    if (course.teacher){
-        let rosterTeacher = document.querySelector('#course-teacher');
-        rosterTeacher.innerHTML = `${course.teacher.honorific} ${course.teacher.name}`;
-    } else {
-        let rosterTeacher = document.querySelector('#course-teacher');
-        rosterTeacher.innerHTML = "Not Set";
-    }
-    // Populate Roster Content
-    for (student of course.students){
-        // Create a new row for the table.
-        let newTR = document.createElement('tr');
+function updateRoster(course) {
+  let rosterTbody = document.querySelector('#roster tbody');
+  // Clear Roster Content
+  rosterTbody.innerHTML = '';
+  if (course.teacher) {
+    let rosterTeacher = document.querySelector('#course-teacher');
+    rosterTeacher.innerHTML = `${course.teacher.honorific} ${course.teacher.name}`;
+  } else {
+    let rosterTeacher = document.querySelector('#course-teacher');
+    rosterTeacher.innerHTML = "Not Set";
+  }
+  // Populate Roster Content
+  for (student of course.students) {
+    // Create a new row for the table.
+    let newTR = document.createElement('tr');
 
-        // Create table cells for each data point and append them to the new row.
-        let nameTD = document.createElement('td');
-        nameTD.innerHTML = student.name;
-        newTR.appendChild(nameTD);
+    // Create table cells for each data point and append them to the new row.
+    let nameTD = document.createElement('td');
+    nameTD.innerHTML = student.name;
+    newTR.appendChild(nameTD);
 
-        let emailTD = document.createElement('td');
-        emailTD.innerHTML = student.email;
-        newTR.appendChild(emailTD);
+    let emailTD = document.createElement('td');
+    emailTD.innerHTML = student.email;
+    newTR.appendChild(emailTD);
 
-        let attendanceTD = document.createElement('td');
-        attendanceTD.innerHTML = student.calculateAttendance();
-        newTR.appendChild(attendanceTD);
+    let attendanceTD = document.createElement('td');
+    attendanceTD.innerHTML = student.calculateAttendance();
+    newTR.appendChild(attendanceTD);
 
-        let actionsTD = document.createElement('td');
-        let presentButton = document.createElement('button');
-        presentButton.innerHTML = "Present";
-        presentButton.setAttribute('data-username', student.username);
-        presentButton.setAttribute('class', 'present');
-        actionsTD.appendChild(presentButton);
+    let actionsTD = document.createElement('td');
+    let presentButton = document.createElement('button');
+    presentButton.innerHTML = "Present";
+    presentButton.setAttribute('data-username', student.username);
+    presentButton.setAttribute('class', 'present');
+    actionsTD.appendChild(presentButton);
 
-        let absentButton = document.createElement('button');
-        absentButton.innerHTML = "Absent";
-        absentButton.setAttribute('data-username', student.username);
-        absentButton.setAttribute('class', 'absent');
-        actionsTD.appendChild(absentButton);
+    let absentButton = document.createElement('button');
+    absentButton.innerHTML = "Absent";
+    absentButton.setAttribute('data-username', student.username);
+    absentButton.setAttribute('class', 'absent');
+    actionsTD.appendChild(absentButton);
 
-        newTR.appendChild(actionsTD);
+    newTR.appendChild(actionsTD);
 
-        // Append the new row to the roster table.
-        rosterTbody.appendChild(newTR);
-    }
-    // Call function to set event listeners on attendance buttons.
-    setupAttendanceButtons();
+    // Append the new row to the roster table.
+    rosterTbody.appendChild(newTR);
+  }
+  // Call function to set event listeners on attendance buttons.
+  setupAttendanceButtons();
 }
 
-function setupAttendanceButtons(){
-    // Set up the event listeners for buttons to mark attendance.
-    let presentButtons = document.querySelectorAll('.present');
-    for (button of presentButtons){
-        button.addEventListener('click', function(e){
-            console.log(`Marking ${e.target.dataset.username} present.`);
-            myCourse.markAttendance(e.target.dataset.username);
-            updateRoster(myCourse);
-        });
-    }
-    let absentButtons = document.querySelectorAll('.absent');
-    for (button of absentButtons){
-        button.addEventListener('click', function(e){
-            console.log(`Marking ${e.target.dataset.username} absent.`);
-            myCourse.markAttendance(e.target.dataset.username, 'absent');
-            updateRoster(myCourse);
-        });
-    }
+function setupAttendanceButtons() {
+  // Set up the event listeners for buttons to mark attendance.
+  let presentButtons = document.querySelectorAll('.present');
+  for (button of presentButtons) {
+    button.addEventListener('click', function(e) {
+      console.log(`Marking ${e.target.dataset.username} present.`);
+      myCourse.markAttendance(e.target.dataset.username);
+      updateRoster(myCourse);
+    });
+  }
+  let absentButtons = document.querySelectorAll('.absent');
+  for (button of absentButtons) {
+    button.addEventListener('click', function(e) {
+      console.log(`Marking ${e.target.dataset.username} absent.`);
+      myCourse.markAttendance(e.target.dataset.username, 'absent');
+      updateRoster(myCourse);
+    });
+  }
 }
-
